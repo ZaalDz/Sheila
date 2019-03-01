e22158ce8c411f52be5a65c275e1035ade08d2fd
Zaali <ZaalDzindzibadze@gmail.com>
2019-03-01 17:57:50 +0400

    fix small bug

--- car/run_car_commands.py
+++ car/run_car_commands.py
@@ -9,18 +9,20 @@ car = Car(pwm_frequency=150)
 
 
 def run_command():
-    receive_command = commands_queue.get()
-    movement_type = receive_command[CommandKeys.MOVEMENT_TYPE]
-    speed = receive_command[CommandKeys.SPEED]
-    move_duration = receive_command[CommandKeys.MOVE_DURATION]
+    while True:
+        receive_command = commands_queue.get()
+        if receive_command:
+            movement_type = receive_command[CommandKeys.MOVEMENT_TYPE]
+            speed = receive_command[CommandKeys.SPEED]
+            move_duration = receive_command[CommandKeys.MOVE_DURATION]
 
-    if movement_type in {MovementType.FORWARD, MovementType.BACKWARD}:
-        car.move(speed, movement_type, move_duration)
+            if movement_type in {MovementType.FORWARD, MovementType.BACKWARD}:
+                car.move(speed, movement_type, move_duration)
 
-    elif movement_type in {MovementType.FORWARD_RIGHT, MovementType.FORWARD_LEFT}:
-        degree = receive_command[CommandKeys.CAR_ROTATION_DEGREE]
-        car.forward_left_right(speed, degree, move_duration)
+            elif movement_type in {MovementType.FORWARD_RIGHT, MovementType.FORWARD_LEFT}:
+                degree = receive_command[CommandKeys.CAR_ROTATION_DEGREE]
+                car.forward_left_right(speed, degree, move_duration)
 
-    elif movement_type in {MovementType.BACKWARD_RIGHT, MovementType.BACKWARD_LEFT}:
-        degree = receive_command[CommandKeys.CAR_ROTATION_DEGREE]
-        car.backward_left_right(speed, degree, move_duration)
+            elif movement_type in {MovementType.BACKWARD_RIGHT, MovementType.BACKWARD_LEFT}:
+                degree = receive_command[CommandKeys.CAR_ROTATION_DEGREE]
+                car.backward_left_right(speed, degree, move_duration)
