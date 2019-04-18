341b58bb4ec88c90ca8a6eb6596de5a990888874
Zaali <zaalidata@gmail.com>
2019-04-18 20:23:56 +0400

    change parameters

--- controller/gather_data.py
+++ controller/gather_data.py
@@ -4,11 +4,11 @@ from time import time
 
 import cv2
 
+from enums import CommandKeys
 from settings import COMMAND_DATA_PATH, FRAME_DATA_PATH, GATHER_DATA
 
 
 def save_data(frame, command, *, image_dir=FRAME_DATA_PATH, command_dir=COMMAND_DATA_PATH, gather_data=GATHER_DATA):
-
     if gather_data is False:
         return
 
@@ -17,7 +17,8 @@ def save_data(frame, command, *, image_dir=FRAME_DATA_PATH, command_dir=COMMAND_
     if not os.path.exists(command_dir):
         os.makedirs(command_dir)
 
-    name = str(time()).replace('.', '')
+    header = command[CommandKeys.MOVEMENT_TYPE]
+    name = f"{header}_{str(time()).replace('.', '')}"
     image_name = os.path.join(image_dir, f'{name}.jpg')
     command_name = os.path.join(command_dir, f'{name}.json')
 
