248e3e6ea893363747439a1ae41f650b6458a114
Zaali <zaalidata@gmail.com>
2019-05-26 00:36:46 +0400

    change pins

--- car/car.py
+++ car/car.py
@@ -7,8 +7,8 @@ class Car:
     def __init__(self, speed: float = 1):
         self.speed = speed
 
-        self.left_motors_1 = Motor(forward=17, backward=27)
-        self.left_motors_2 = Motor(forward=22, backward=10)
+        self.left_motors_1 = Motor(forward=24, backward=25)
+        self.left_motors_2 = Motor(forward=18, backward=23)
 
         self.right_motors_1 = Motor(forward=12, backward=16)
         self.right_motors_2 = Motor(forward=20, backward=21)
